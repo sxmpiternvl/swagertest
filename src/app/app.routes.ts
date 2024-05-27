099bb0b98a8664cbc26220bbff03c216adc69453
sxmpiternvl <136491031+sxmpiternvl@users.noreply.github.com>
2024-05-27 13:05:33 +0500

    err

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import {Routes} from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
-import {LayoutComponent} from "./pages/layout/layout.component";
-import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 
 export const routes: Routes = [
   {
@@ -10,13 +8,5 @@ export const routes: Routes = [
   {
     path: 'login', component: LoginComponent
   },
-  {
-    path: '', component: LayoutComponent,
-    children: [
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      }
-    ]
-  },
+
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,35 +1,34 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, HttpClientModule],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  loginObj: Login;
+  loginObj: Login = new Login();
 
-  constructor(private http: HttpClient) {
-    this.loginObj = new Login();
-  }
+  constructor(private http: HttpClient) { }
 
   onLogin() {
-    this.http.post('http://localhost:3000/api/User/Login', this.loginObj).subscribe(
-      (res: any) => {
-        if (res.result) {
-          alert("LOGIN SUCCESS");
-        } else {
-          alert(res.message);
+    this.http.post('http://localhost:3000/api/User/Login', this.loginObj, { observe: 'response' }).subscribe(
+      {
+        next: (data: HttpResponse<any>) => {
+          console.log(data);
+          alert((data.status + data.body.message));
+        },
+        error: (err) => {
+          alert(err.error.cod + err.error.message);
         }
-      },
+      }
     );
   }
 }
-
 export class Login {
   EmailId: string;
   Password: string;
